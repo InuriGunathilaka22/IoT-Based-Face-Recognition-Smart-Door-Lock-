60a886bb3b4d9528d19fda74a1da246ebf291324
InuriGunathilaka22 <inurilee99@gmail.com>
2023-05-09 02:09:15 +0530

    Add .gitattributes file to prevent line ending normalization

---
+++ Code_FaceRecognitionDoorLock_ESP32CAM_01/Code_FaceRecognitionDoorLock_ESP32CAM_01.ino
@@ -0,0 +1,162 @@
+/**********************************************************************************
+ *  TITLE: ESP32-CAM Face Recognition Door Lock System
+ *  Click on the following links to learn more. 
+ *  YouTube Video: https://youtu.be/PBZ-EiVIUc4
+ *  Related Blog : https://easyelectronicsproject.com/projects/esp32-projects/
+ *  by Tech StudyCell
+ **********************************************************************************/
+
+/**********************************************************************************
+ *  Preferences--> Aditional boards Manager URLs : https://dl.espressif.com/dl/package_esp32_index.json, http://arduino.esp8266.com/stable/package_esp8266com_index.json
+ *  Board Settings:
+ *  Board: "ESP32 Wrover Module"
+ *  Upload Speed: "921600"
+ *  Flash Frequency: "80MHz"
+ *  Flash Mode: "QIO"
+ *  Partition Scheme: "Hue APP (3MB No OTA/1MB SPIFFS)"
+ *  Core Debug Level: "None"
+ *  COM Port: Depends *On Your System*
+ *  
+ *  GPIO 0 must be connected to GND pin while uploading the sketch
+ *  After connecting GPIO 0 to GND pin, press the ESP32 CAM on-board RESET button to put the board in flashing mode
+ ***************************************************************************************/
+
+
+#include "esp_camera.h"
+#include <WiFi.h>
+//
+// WARNING!!! PSRAM IC required for UXGA resolution and high JPEG quality
+//            Ensure ESP32 Wrover Module or other board with PSRAM is selected
+//            Partial images will be transmitted if image exceeds buffer size
+//
+
+// Select camera model
+//#define CAMERA_MODEL_WROVER_KIT
+//#define CAMERA_MODEL_ESP_EYE
+//#define CAMERA_MODEL_M5STACK_PSRAM
+//#define CAMERA_MODEL_M5STACK_WIDE
+#define CAMERA_MODEL_AI_THINKER
+
+#include "camera_pins.h"
+
+#define RED 13
+#define GREEN 14
+#define LOCK 12
+
+const char* ssid = "WIFI NAME"; //WiFi SSID
+const char* password = "WIFI PASSWORD"; //WiFi Password
+
+void startCameraServer();
+
+boolean matchFace = false;
+boolean openLock = false;
+long prevMillis=0;
+int interval = 6000;  //DELAY
+
+void setup() {
+  pinMode(LOCK,OUTPUT);
+  pinMode(RED,OUTPUT);
+  pinMode(GREEN,OUTPUT);
+  digitalWrite(LOCK,LOW);
+  digitalWrite(RED,HIGH);
+  digitalWrite(GREEN,LOW);
+  
+  Serial.begin(115200);
+  Serial.setDebugOutput(true);
+  Serial.println();
+
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+  //init with high specs to pre-allocate larger buffers
+  if(psramFound()){
+    config.frame_size = FRAMESIZE_UXGA;
+    config.jpeg_quality = 10;
+    config.fb_count = 2;
+  } else {
+    config.frame_size = FRAMESIZE_SVGA;
+    config.jpeg_quality = 12;
+    config.fb_count = 1;
+  }
+
+#if defined(CAMERA_MODEL_ESP_EYE)
+  pinMode(13, INPUT_PULLUP);
+  pinMode(14, INPUT_PULLUP);
+#endif
+
+  // camera init
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return;
+  }
+
+  sensor_t * s = esp_camera_sensor_get();
+  //initial sensors are flipped vertically and colors are a bit saturated
+  if (s->id.PID == OV3660_PID) {
+    s->set_vflip(s, 1);//flip it back
+    s->set_brightness(s, 1);//up the blightness just a bit
+    s->set_saturation(s, -2);//lower the saturation
+  }
+  //drop down frame size for higher initial frame rate
+  s->set_framesize(s, FRAMESIZE_QVGA);
+
+#if defined(CAMERA_MODEL_M5STACK_WIDE)
+  s->set_vflip(s, 1);
+  s->set_hmirror(s, 1);
+#endif
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+
+  startCameraServer();
+
+  Serial.print("Camera Ready! Use 'http://");
+  Serial.print(WiFi.localIP());
+  Serial.println("' to connect");
+}
+
+void loop() {
+  if(matchFace==true && openLock==false)
+  {
+    openLock=true;
+    digitalWrite(LOCK,HIGH);
+    digitalWrite(GREEN,HIGH);
+    digitalWrite(RED,LOW);
+    prevMillis=millis();
+    Serial.print("UNLOCK DOOR");    
+   }
+   if (openLock == true && millis()-prevMillis > interval)
+   {
+    openLock=false;
+    matchFace=false;
+    digitalWrite(LOCK,LOW);
+    digitalWrite(GREEN,LOW);
+    digitalWrite(RED,HIGH);
+    Serial.print("LOCK DOOR");
+    }
+}
